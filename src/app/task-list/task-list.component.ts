fcee6eeb6047f885c575ab632e263f02d6616200
Hjjj Jjj <sebgarridor@gmail.com>
2023-01-30 12:47:30 -0300

    moving into different components

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+
+
+  tasks: Task[] = [
+    new Task("Visit Ann"),
+    new Task("Call Dad"),
+    new Task ("Go to the gym"),
+    new Task("Wash the dishes"),
+    new Task("Shop for the party")
+  ]
+
+  addNewTask(newTask: string){
+    this.tasks.push(new Task(newTask));
+  }
+
+  removeTask(existingTask: Task){
+
+    //creating a warning message to the user
+    var userConfirmed = confirm(`Are you sure you wanna remove the following task? \n "${existingTask.title}"`)
+    
+
+    //validation. if the user said 'yes', we proceed.
+    if(userConfirmed == true) {
+
+      //we filter the entire array so it returns the same array but without the value of "existingTask"
+      this.tasks = this.tasks.filter(task => task !=existingTask)
+    }
+  }
+
+
+}
+
+class Task {
+  constructor(public title: string){
+    
+  }
+
+  toggleIsDone(){
+    
+    this.isDone = !this.isDone;
+  }
+
+  public isDone = false;
+
+}
+
+
