1d7e6c3313ec0d9680a62bb35bb3385033939dd2
Hjjj Jjj <sebgarridor@gmail.com>
2023-01-30 13:42:57 -0300

    adding calendar and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
+import { CalendarModule } from './calendar/calendar.module';
+import { TaskListModule } from './task-list/task-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaskListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TaskListModule,
+    CalendarModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendar/calendar.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarComponent } from './calendar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule} from '@angular/material/datepicker'
+import { MatNativeDateModule} from '@angular/material/core'
+
+@NgModule({
+  declarations: [
+    CalendarComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+    
+  ],
+  exports : [
+    CalendarComponent
+  ]
+})
+export class CalendarModule { }
